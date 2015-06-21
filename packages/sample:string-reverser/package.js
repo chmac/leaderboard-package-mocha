b0f55322644d24454c92a5ca3dc2a712f61f8036
Callum Macdonald <git@callum-macdonald.com>
2015-06-21 13:14:25 +0200

    Break tests by loading mocha only on server.

--- packages/sample:string-reverser/package.js
+++ packages/sample:string-reverser/package.js
@@ -1,7 +1,5 @@
 Package.onTest(function (api) {
-  api.use(['mike:mocha-package@0.5.6', "practicalmeteor:chai"]);
+  api.use(['mike:mocha-package@0.5.6', "practicalmeteor:chai"], ['server']);
 
-  api.addFiles('tests/shared/shared.js', ['client', 'server']);
-  api.addFiles('tests/client/client.js', ['client']);
   api.addFiles('tests/server/server.js', ['server']);
 });
